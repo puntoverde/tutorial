631e0b29aec10dc47ceb5df46cbab3b3dab14293
Stefan Hupe <stefan.hupe@campanori.com>
2013-09-01 20:25:57 +0200

    eeee

---
+++ app/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* @category Ich
+* @package  Ich
+* @author   ich <ich@ich.ich>
+* @license  ich
+* @link     ich URL http://ich.ich GPL
+*/
+
+/**
+ * test
+ *
+ * Das hier ist die extra Beschreibung
+ *
+ * @version 2013-08-31
+ * @since 2013-08-31
+ * @author shu
+ * @copyright campanori
+ * @license gpl
+ * @example Beispiel
+ * @global string $global Variable
+ * @param int $zahl Ganzzahl
+ * @return int Ergebnis
+ * @brief Kurzfassung
+ */
+
+function test()
+{
+
+}
