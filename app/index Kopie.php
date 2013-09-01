dc45be36f7c3a3866a546c217570b0c936e17a99
Stefan Hupe <stefan.hupe@campanori.com>
2013-09-01 17:27:54 +0200

    test

---
+++ app/index Kopie.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* @category Ich
+* @package  Ich
+* @author   ich <ich@ich.ich>
+* @license  ich
+* @link     ich URL http://ich.ich GPL
+*/
+
+echo "Hallo";
+
+if ($file="mehr") {
+    $file="mehr";
+}
